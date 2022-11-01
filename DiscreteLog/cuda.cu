b55d98c29b0b4f30d6a07b861a56341f0bc5f5bf
Andres Ricardo Perez Rojas <riperezro@unal.edu.co>
2022-11-01 08:27:53 -0500

    Function1 and sort methods

--- DiscreteLog/cuda.cu
+++ DiscreteLog/cuda.cu
@@ -19,6 +19,10 @@ string print(i128 x) {
     return ret;
 }
 
+i128 ceil_division(i128 num, i128 den){
+    return (num + den - (i128)1) / den;
+}
+
 i128 hostFastExpo( i128 &base, i128 &expo, i128 &m ){
     if( expo == 0 ) return 1;
     i128 prv_expo = expo/(i128)2;
@@ -37,28 +41,123 @@ __device__ i128 fastExpo( i128 base, i128 expo, i128 m ){
     return ret;
 }
 
-// __global__ void calculateFunction1(thrust::device_vector<long long> &results){
-__global__ void calculateFunction1(){
-    i128 var = fastExpo(5,3,37);
-    printf("%d\n", (int) var);
+__device__ i128 function_1( i128 a, i128 n, i128 p, i128 m ){
+    i128 expo = (n * p)%m;
+    return fastExpo(a, expo, m);
+}
+
+__global__ void calculateFunction1(long long *a, long long *m, long long *n, long long *limit, long long *step, 
+                                    long long *results, long long *keys){
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    int numThreads = gridDim.x * blockDim.x;
+    printf("IN THREAD %d\n", threadId);
+
+    i128 low = (*step) * threadId + (i128)1;
+    i128 high = low + (*step) - (i128)1;
+    if( threadId + 1 == numThreads ) high = (*limit);
+    long long results_index = threadId * (*step);
+    printf("MIDDLE THREAD %d\n", threadId);
+    for(i128 p = low ; p <= high ; p ++) {
+        i128 value = function_1((*a), (*n), p, (*m));
+        // INSERT TO RESULTS
+        printf("\tPut %d -> %d in %d\n", (int) p, (int) value, (int) results_index);
+        results[results_index] = (long long) p;
+        keys[results_index] = (long long) value;
+        results_index ++;
+    }
+    printf("OUT THREAD %d\n", threadId);
 }
 
 __global__ void calculateFunction2(){
     long long var = fastExpo(5,3,37);
 }
 
+__global__ void print_arrays(long long *limit, long long *results, long long *keys){
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    // int numThreads = gridDim.x * blockDim.x;
+    printf("IN PRINT THREAD %d\n", threadId);
+    for(int i = 0 ; i < (*limit) ; i ++){
+        printf("\tpos %d -> %d :: %d\n",i, (int)keys[i], (int)results[i]);
+    }
+    printf("OUT PRINT THREAD %d\n", threadId);
+}
+
 int main(){
 
-    // i128 a = 56439;
-    // i128 gen_x = 15432465;
-    // i128 m = 29996224275833;
-    // i128 b = hostFastExpo(a, gen_x, m);
+    i128 a = 5;
+    i128 gen_x = 14;
+    i128 m = 37;
+    i128 b = hostFastExpo(a, gen_x, m);
 
     int var_a = 22;
     printf("TEST %d\n",var_a);
 
-    calculateFunction1<<<2,2>>>();
+    int blocks = 2;
+    int threadsPerBlock = 2;
+    int numThreads = blocks * threadsPerBlock;
+
+    // Host variables
+    long long host_a = a;
+    long long host_b = b;
+    long long host_m = m;
+    long long host_n = sqrt((long double) m);
+    long long host_limit = ceil_division(host_m,host_n);
+    long long host_step = host_limit / (long long) numThreads;
+    int var_size = sizeof( long long );
+    int array_value_size = host_n + 5;
+    int array_size = array_value_size * sizeof( long long );
+
+    // Device variables
+    long long *device_a;
+    long long *device_b;
+    long long *device_m;
+    long long *device_n;
+    long long *device_limit;
+    long long *device_step;
+    long long *device_results;
+    long long *device_keys;
+
+    cudaMalloc( (void**)&device_a , var_size );
+    cudaMalloc( (void**)&device_b , var_size );
+    cudaMalloc( (void**)&device_m , var_size );
+    cudaMalloc( (void**)&device_n , var_size );
+    cudaMalloc( (void**)&device_limit , var_size );
+    cudaMalloc( (void**)&device_step , var_size );
+    cudaMalloc( (void**)&device_results , array_size );
+    cudaMalloc( (void**)&device_keys , array_size );
+
+    cudaMemcpy( device_a , &host_a , var_size , cudaMemcpyHostToDevice );
+    cudaMemcpy( device_b , &host_b , var_size , cudaMemcpyHostToDevice );
+    cudaMemcpy( device_m , &host_m , var_size , cudaMemcpyHostToDevice );
+    cudaMemcpy( device_n , &host_n , var_size , cudaMemcpyHostToDevice );
+    cudaMemcpy( device_limit , &host_limit , var_size , cudaMemcpyHostToDevice );
+    cudaMemcpy( device_step , &host_step , var_size , cudaMemcpyHostToDevice );
+
+    // Call the Function 1 kernel
+
+    calculateFunction1<<<blocks,threadsPerBlock>>>(
+        device_a, device_m, device_n, device_limit, device_step,
+        device_results, device_keys);
     cudaDeviceSynchronize();
+
+    // Sort results and keys
+    thrust::device_ptr<long long> thrust_keys(device_keys);
+    thrust::device_ptr<long long> thrust_results(device_results);
+    thrust::sort_by_key(thrust::device, thrust_keys, thrust_keys + host_limit, thrust_results);
+    cudaDeviceSynchronize();
+
+    print_arrays<<<1,1>>>(device_limit, device_results, device_keys);
+    cudaDeviceSynchronize();
+
+    // Free device memory
+    cudaFree( device_a );
+    cudaFree( device_b );
+    cudaFree( device_m );
+    cudaFree( device_n );
+    cudaFree( device_limit );
+    cudaFree( device_step );
+    cudaFree( device_results );
+    cudaFree( device_keys );
 
     printf("POST TEST\n");
     

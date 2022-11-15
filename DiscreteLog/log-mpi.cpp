3948833f03089056e0f8886a39f902816b4d3d34
Andres Ricardo Perez Rojas <andresricardoperez@gmail.com>
2022-11-15 16:57:14 -0500

    Add mpi source file

---
+++ DiscreteLog/log-mpi.cpp
@@ -0,0 +1,160 @@
+// mpicxx -o log-mpi log-mpi.cpp `pkg-config --cflags --libs opencv`
+// mpirun -np 4 log-mpi
+#include <bits/stdc++.h>
+#include <sys/time.h>
+
+using namespace std;
+typedef unsigned __int128 i128;
+
+i128 fastExpo( i128 &base, i128 &expo, i128 &m ){
+    if( expo == 0 ) return 1;
+    i128 prv_expo = expo/(i128)2;
+    i128 ret = fastExpo(base, prv_expo, m);
+    ret = (ret * ret)%m;
+    if( expo%(i128)2 ) ret = (ret * base)%m;
+    return ret;
+}
+
+i128 function_1( i128 &a, i128 &n, i128 &p, i128 &m ){
+    i128 expo = (n * p)%m;
+    return fastExpo(a, expo, m);
+}
+
+i128 function_2( i128 &a, i128 &b, i128 &q, i128 &m ){
+    i128 ret = fastExpo(a, q, m);
+    ret = (ret * b)%m;
+    return ret;
+}
+
+i128 ceil_division(i128 num, i128 den){
+    return (num + den - (i128)1) / den;
+}
+
+int rank, size;
+const int array_size = 5476880 + 100;
+long long f1_values[array_size];
+long long f1_keys[array_size];
+long long local_f1_values[array_size];
+long long local_f1_keys[array_size];
+i128 a, b, m, n;
+long long x, proc_x;
+
+void calculateFunction1(long long low, long long high){
+    int position = 0;
+    for(i128 p = low ; p <= high ; p ++) {
+        long long value = function_1(a, b, p, m);
+        local_f1_values[position] = value;
+        local_f1_keys[position] = (long long) p;
+        position ++;
+    }
+}
+
+long long getEqualResult(long long limit, long long target){
+    int low = 0;
+    int high = limit-1;
+    int middle;
+    while( low < high ){
+        middle = (low+high+1)/2;
+        if( f1_values[middle] <= target ) low = middle;
+        else high = middle-1;
+    }
+    if(f1_values[low] == target) return f1_keys[low];
+    return -1;
+}
+
+void calculateFunction2(long long low, long long high){
+    x = -1;
+    for(i128 q = low ; q <= high ; q ++) {
+        long long value = function_2(a, b, q, m);
+        long long findP = getEqualResult(array_size, value);
+        if( findP == -1 ) continue;
+        i128 currentX = (n * (i128)findP)%( m );
+        currentX = (currentX - q + m)%( m );
+        proc_x = (long long) currentX;
+    }
+}
+
+bool sort_by_value( int a , int b ){
+    return f1_values[a] < f1_values[b];
+}
+
+void sort_arrays(){
+    int indexes[array_size];
+    long long f1_values_copy[array_size];
+    long long f1_keys_copy[array_size];
+    for( int i = 0 ; i < array_size ; i ++ ){
+        indexes[i] = i;
+        f1_values_copy[i] = f1_values[i];
+        f1_keys_copy[i] = f1_keys[i];
+    }
+    sort(indexes, indexes+array_size, sort_by_value);
+    for( int i = 0 ; i < array_size ; i ++ ){
+        f1_values[i] = f1_values_copy[indexes[i]];
+        f1_values[i] = f1_keys_copy[indexes[i]];
+    }
+}
+
+int main(int argc, char* argv[]){
+
+    MPI_Init( &argc, &argv );
+        int root = 0;
+        MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+        MPI_Comm_size( MPI_COMM_WORLD, &size );
+
+        a = 56439;
+        b = 27341644544150;
+        m = 29996224275833;
+        n = sqrt((long double) m);
+    
+        struct timeval tval_before, tval_after, tval_result;
+
+        if( rank == root ){
+            gettimeofday(&tval_before, NULL);
+        }
+
+        long long limit = ceil_division(m,n);
+        long long step = limit/(long long)size;
+        long long low = (long long)1 + ((long long) rank * step);
+        long long high = low + step - (long long)1;
+        if( rank + 1 == size ) high = limit;
+
+        for(int i = 0 ; i < step+2 ; i ++){
+            local_f1_values[i] = LLONG_MAX;
+            local_f1_keys[i] = LLONG_MAX;
+        }
+
+        calculateFunction1(low, high);
+        MPI_Gather ( local_f1_values, (int) step+2, MPI_LONG_LONG_INT, f1_values, (int)(step+2)*size,
+                    MPI_LONG_LONG_INT, root, MPI_COMM_WORLD );
+        MPI_Gather ( local_f1_keys, (int) step+2, MPI_LONG_LONG_INT, f1_keys, (int)(step+2)*size,
+                    MPI_LONG_LONG_INT, root, MPI_COMM_WORLD );
+
+        if( rank == root ){
+            sort_arrays();
+        }
+
+        MPI_Bcast( f1_values, array_size, MPI_LONG_LONG_INT, root, MPI_COMM_WORLD);
+        MPI_Bcast( f1_keys, array_size, MPI_LONG_LONG_INT, root, MPI_COMM_WORLD);
+
+        limit = n;
+        step = limit/(long long)size;
+        low = (long long) rank * step;
+        high = low + step - (long long)1;
+        if( rank + 1 == size ) high = limit;
+        calculateFunction2();
+
+        MPI_Reduce(&proc_x, &x, 1, MPI_LONG_LONG_INT, MPI_MAX, root, MPI COMM WORLD);
+
+        if( rank == root ){
+            printf("FOUND X=%lld\n", x);
+            gettimeofday(&tval_after, NULL);
+            timersub(&tval_after, &tval_before, &tval_result);
+            printf("%ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+        }
+    MPI_Finalize( );
+    
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+
+    assert(fastExpo(a,x,m) == b);
+}
